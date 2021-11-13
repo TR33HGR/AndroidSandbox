18a6e37a255de97c50f68105479db16acf5ffd32
Nathaniel Micklewright <nathaniel.john.m@gmail.com>
2021-11-13 16:08:06 +0900

    Button press opens new activity

--- app/src/main/java/com/example/androidsandbox/MainActivity.kt
+++ app/src/main/java/com/example/androidsandbox/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.androidsandbox
 
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
 
 const val DEBUG = "DEBUG"
+
+const val EXTRA_MESSAGE = "com.example.androidsandbox.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +20,10 @@ class MainActivity : AppCompatActivity() {
     fun sendMessage(view: View) {
         val message = findViewById<EditText>(R.id.editTextTextPersonName).text.toString()
         Log.d(DEBUG, message)
+
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 }
