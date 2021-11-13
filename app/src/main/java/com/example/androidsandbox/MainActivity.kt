4871f55617ada7d9ba6efd28e5c81a564cfb2786
Nathaniel Micklewright <nathaniel.john.m@gmail.com>
2021-11-13 15:58:24 +0900

    Print Input message to Logcat on button press

--- app/src/main/java/com/example/androidsandbox/MainActivity.kt
+++ app/src/main/java/com/example/androidsandbox/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.androidsandbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+
+const val DEBUG = "DEBUG"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sendMessage(view: View) {
+        val message = findViewById<EditText>(R.id.editTextTextPersonName).text.toString()
+        Log.d(DEBUG, message)
     }
 }
