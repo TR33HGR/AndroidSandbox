3587024724b9befeee5aa7d723a9c07b5fdba2d8
Nathaniel Micklewright <nathaniel.john.m@gmail.com>
2021-11-14 12:10:32 +0900

    Use Volley to make a request to google

--- app/src/main/java/com/example/androidsandbox/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/androidsandbox/DisplayMessageActivity.kt
@@ -3,15 +3,23 @@ package com.example.androidsandbox
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class DisplayMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
 
-        val message = intent.getStringExtra(EXTRA_MESSAGE)
-        val textView = findViewById<TextView>(R.id.textView).apply {
-            text = message
-        }
+        val textView = findViewById<TextView>(R.id.textView)
+
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://www.google.com"
+
+        val stringRequest = StringRequest(Request.Method.GET, url, { response ->
+            textView.text = "Response is: ${response.substring(0, 500)}"
+        }, { textView.text = "That didn't work!" })
+        queue.add(stringRequest)
     }
 }
